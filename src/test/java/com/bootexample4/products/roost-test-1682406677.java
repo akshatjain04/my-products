6b4312ad6c48c8e65a063b11391f748e577578fb
akshatjain04 <akshatjain0410@gmail.com>
2023-04-25 07:13:09 +0000

    Test case generated by Roost.AI

---
+++ src/test/java/com/bootexample4/products/roost-test-1682406677.java
@@ -0,0 +1,117 @@
+package com.bootexample4.products.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import com.bootexample4.products.model.Product;
+import com.bootexample4.products.repository.ProductRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductControllerTest {
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private Product product1;
+    private Product product2;
+
+    @BeforeEach
+    public void setUp() {
+        product1 = new Product(1L, "Product 1", "Description 1", 100.0);
+        product2 = new Product(2L, "Product 2", "Description 2", 200.0);
+    }
+
+    @Test
+    public void testGetAllProducts() {
+        when(productRepository.findAll()).thenReturn(Arrays.asList(product1, product2));
+
+        List<Product> products = productController.getAllProducts();
+
+        assertThat(products).isNotEmpty();
+        assertThat(products).hasSize(2);
+        assertThat(products).contains(product1, product2);
+    }
+
+    @Test
+    public void testCreateProduct() {
+        when(productRepository.save(any(Product.class))).thenReturn(product1);
+
+        Product createdProduct = productController.createProduct(product1);
+
+        assertThat(createdProduct).isEqualTo(product1);
+    }
+
+    @Test
+    public void testGetProductById() {
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product1));
+
+        ResponseEntity<Product> response = productController.getProductById(1L);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody()).isEqualTo(product1);
+    }
+
+    @Test
+    public void testGetProductByIdNotFound() {
+        when(productRepository.findById(1L)).thenReturn(Optional.empty());
+
+        ResponseEntity<Product> response = productController.getProductById(1L);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(404);
+    }
+
+    @Test
+    public void testUpdateProduct() {
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product1));
+        when(productRepository.save(any(Product.class))).thenReturn(product1);
+
+        ResponseEntity<Product> response = productController.updateProduct(1L, product1);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody()).isEqualTo(product1);
+    }
+
+    @Test
+    public void testUpdateProductNotFound() {
+        when(productRepository.findById(1L)).thenReturn(Optional.empty());
+
+        ResponseEntity<Product> response = productController.updateProduct(1L, product1);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(404);
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product1));
+
+        ResponseEntity<Object> response = productController.deleteProduct(1L);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        verify(productRepository, times(1)).delete(product1);
+    }
+
+    @Test
+    public void testDeleteProductNotFound() {
+        when(productRepository.findById(1L)).thenReturn(Optional.empty());
+
+        ResponseEntity<Object> response = productController.deleteProduct(1L);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(404);
+    }
+}
---
+++ src/test/java/com/bootexample4/products/roost-test-1682406715.java
@@ -0,0 +1,43 @@
+import com.bootexample4.products.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductTest {
+
+    private Product product;
+
+    @BeforeEach
+    public void setUp() {
+        product = new Product();
+    }
+
+    @Test
+    public void testGetAndSetId() {
+        Long id = 1L;
+        product.setId(id);
+        assertEquals(id, product.getId());
+    }
+
+    @Test
+    public void testGetAndSetName() {
+        String name = "Test Product";
+        product.setName(name);
+        assertEquals(name, product.getName());
+    }
+
+    @Test
+    public void testGetAndSetDescription() {
+        String description = "This is a test product";
+        product.setDescription(description);
+        assertEquals(description, product.getDescription());
+    }
+
+    @Test
+    public void testGetAndSetPrice() {
+        double price = 99.99;
+        product.setPrice(price);
+        assertEquals(price, product.getPrice());
+    }
+}
