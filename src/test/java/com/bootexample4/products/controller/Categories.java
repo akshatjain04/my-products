21006a8f1f8edea51989963fcdc3ec34cc5b464d
akshatjain04 <akshatjain0410@gmail.com>
2024-07-08 11:07:26 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4-1106-preview

---
+++ src/test/java/com/bootexample4/products/controller/Categories.java
@@ -0,0 +1,25 @@
+package com.bootexample4.products.controller;
+
+public interface Categories {
+
+	interface getAllProducts {
+
+	}
+
+	interface createProduct {
+
+	}
+
+	interface getProductById {
+
+	}
+
+	interface updateProduct {
+
+	}
+
+	interface deleteProduct {
+
+	}
+
+}
---
+++ src/test/java/com/bootexample4/products/model/Categories.java
@@ -0,0 +1,37 @@
+package com.bootexample4.products.model;
+
+public interface Categories {
+
+	interface getId {
+
+	}
+
+	interface setId {
+
+	}
+
+	interface getName {
+
+	}
+
+	interface setName {
+
+	}
+
+	interface getDescription {
+
+	}
+
+	interface setDescription {
+
+	}
+
+	interface getPrice {
+
+	}
+
+	interface setPrice {
+
+	}
+
+}
