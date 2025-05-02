c9ccd523f4b2036072d9c0692df220258937bfe7
akshatjain04 <akshat.jain@zb.io>
2025-05-02 14:30:32 +0530

    API test (Using API Spec) generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/bootexample4/api_tests/ZbIoCredentialWalletApiRefsHeadsMainApiCredentialregisterWallet100ResolvedTest.java
@@ -0,0 +1,31 @@
+
+package com.bootexample4.api_tests;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ZbIoCredentialWalletApiRefsHeadsMainApiCredentialregisterWallet100ResolvedTest {
+
+	@Test
+	void testAll() {
+		String apiHostServer = System.getenv()
+			.getOrDefault("url2_credentialregister-wallet-1.0.0-resolved_URL_BASE",
+					"https://virtserver.swaggerhub.com/credentialregister/wallet/1.0.0");
+		String url2credentialregisterwallet100resolvedauthtoken = System.getenv()
+			.getOrDefault("url2_credentialregister-wallet-1.0.0-resolved_AUTH_TOKEN", "auth2");
+		Results results = Runner.path(
+				"src/test/java/com/bootexample4/api_tests/ZbIoCredentialWalletApiRefsHeadsMainApiCredentialregisterWallet100Resolved")
+			.systemProperty("url2_credentialregister-wallet-1.0.0-resolved_URL_BASE", apiHostServer)
+			.systemProperty("url2_credentialregister-wallet-1.0.0-resolved_AUTH_TOKEN",
+					url2credentialregisterwallet100resolvedauthtoken)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
