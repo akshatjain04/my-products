6b87c2349faa2eeac01b9f6b5f5e9550f3a462f5
akshatjain04 <akshat.jain@zb.io>
2025-05-09 03:13:13 +0530

    API test (Using API Spec) generated by RoostGPT
    Using AI Model roostgpt-4-32k

--- src/test/java/com/bootexample4/api_tests/TresourcePoolManagement2Test.java
+++ src/test/java/com/bootexample4/api_tests/TresourcePoolManagement2Test.java
@@ -13,11 +13,11 @@
 
       @Test
       void testAll() {
-          String apiHostServer = System.getenv().getOrDefault("url1_tresourcePoolManagement_(2)_URL_BASE", "http://localhost:4010");
-String url1tresourcepoolmanagement2authtoken = System.getenv().getOrDefault("url1_tresourcePoolManagement_(2)_AUTH_TOKEN", "dummy_url1_tresourcePoolManagement_(2)_AUTH_TOKEN");
+          String apiHostServer = System.getenv().getOrDefault("url1_tresourcePoolManagement_(2)_URL1_TRESOURCEPOOLMANAGEMENT__2__URL_BASE", "http://localhost:4010");
+String url1tresourcepoolmanagement2url1tresourcepoolmanagement2authtoken = System.getenv().getOrDefault("url1_tresourcePoolManagement_(2)_URL1_TRESOURCEPOOLMANAGEMENT__2__AUTH_TOKEN", "dummy_url1_tresourcePoolManagement_(2)_URL1_TRESOURCEPOOLMANAGEMENT__2__AUTH_TOKEN");
           Results results = Runner.path("src/test/java/com/bootexample4/api_tests/TresourcePoolManagement2")
-                  .systemProperty("url1_tresourcePoolManagement_(2)_URL_BASE", apiHostServer)
-.systemProperty("url1_tresourcePoolManagement_(2)_AUTH_TOKEN", url1tresourcepoolmanagement2authtoken)
+                  .systemProperty("url1_tresourcePoolManagement_(2)_URL1_TRESOURCEPOOLMANAGEMENT__2__URL_BASE", apiHostServer)
+.systemProperty("url1_tresourcePoolManagement_(2)_URL1_TRESOURCEPOOLMANAGEMENT__2__AUTH_TOKEN", url1tresourcepoolmanagement2url1tresourcepoolmanagement2authtoken)
                   .reportDir("testReport").parallel(1);
           assertEquals(0, results.getFailCount(), results.getErrorMessages());
       }
