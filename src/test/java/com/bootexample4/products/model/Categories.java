de7377fec0fe5e3c53b61128d2ccb5822a58aa43
akshatjain04 <akshat.jain@zb.io>
2025-05-12 12:05:08 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/bootexample4/products/model/Categories.java
@@ -0,0 +1,25 @@
+package com.bootexample4.products.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getId {
+
+	}
+
+}
