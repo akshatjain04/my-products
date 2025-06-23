d0a1100c68aa9f494d50c78152b75c9bb1787cdc
akshatjain04 <akshat.jain@zb.io>
2025-06-23 12:50:09 +0000

    API test (Using API Spec) generated by RoostGPT
    Using AI Model Inference

---
+++ src/test/java/com/bootexample4/api_tests/V2SwaggerTest.java
@@ -0,0 +1,26 @@
+
+package com.bootexample4.api_tests;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class V2SwaggerTest {
+
+	@Test
+	void testAll() {
+		String urlbase = System.getenv().getOrDefault("url.base", "dummy_url.base");
+		String authtoken = System.getenv().getOrDefault("AUTH_TOKEN", "dummy_AUTH_TOKEN");
+		Results results = Runner.path("src/test/java/com/bootexample4/api_tests/V2Swagger")
+			.systemProperty("url.base", urlbase)
+			.systemProperty("AUTH_TOKEN", authtoken)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
