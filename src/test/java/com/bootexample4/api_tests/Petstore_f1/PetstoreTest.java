dc592f19300d3f27329daac3efb462399407a7c9
akshatjain04 <akshat.jain@zb.io>
2025-06-27 02:43:09 +0530

    API test (Using API Spec) generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/bootexample4/api_tests/Petstore_f1/PetstoreTest.java
@@ -0,0 +1,28 @@
+
+package com.bootexample4.api_tests;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PetstoreTest {
+
+	@Test
+	void testAll() {
+		String petstoreapihost = System.getenv().get("PETSTORE_API_HOST");
+		String petstoreapikey = System.getenv().get("PETSTORE_API_KEY");
+		String petstoreauthtoken = System.getenv().get("PETSTORE_AUTH_TOKEN");
+		Results results = Runner.path("src/test/java/com/bootexample4/api_tests/Petstore_f1")
+			.systemProperty("PETSTORE_API_HOST", petstoreapihost)
+			.systemProperty("PETSTORE_API_KEY", petstoreapikey)
+			.systemProperty("PETSTORE_AUTH_TOKEN", petstoreauthtoken)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
