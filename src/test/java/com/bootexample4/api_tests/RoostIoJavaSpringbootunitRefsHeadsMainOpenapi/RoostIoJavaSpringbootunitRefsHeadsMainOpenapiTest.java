7c0783ad6fa10e2a3e0765f6572b6a6119a56907
akshatjain04 <akshat.jain@zb.io>
2025-07-03 16:50:33 +0530

    API test (Using API Spec) generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/bootexample4/api_tests/RoostIoJavaSpringbootunitRefsHeadsMainOpenapi/RoostIoJavaSpringbootunitRefsHeadsMainOpenapiTest.java
@@ -0,0 +1,25 @@
+
+package com.bootexample4.api_tests.RoostIoJavaSpringbootunitRefsHeadsMainOpenapi;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RoostIoJavaSpringbootunitRefsHeadsMainOpenapiTest {
+
+	@Test
+	void testAll() {
+		String myproductsbaseurl = System.getenv().get("MY_PRODUCTS_BASE_URL");
+		Results results = Runner
+			.path("src/test/java/com/bootexample4/api_tests/RoostIoJavaSpringbootunitRefsHeadsMainOpenapi")
+			.systemProperty("MY_PRODUCTS_BASE_URL", myproductsbaseurl)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
